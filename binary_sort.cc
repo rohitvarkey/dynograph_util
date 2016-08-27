19b0cefbe07b3643bde18a3c7f938017c22107cc
Eric Hein <ehein6@gatech.edu>
2016-08-27 13:03:16 -0400

    Added standalone exe for sorting graph inputs in binary format.

---
+++ binary_sort.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "dynograph_util.hh"
+using std::cout;
+using std::cerr;
+using std::vector;
+using std::sort;
+
+using namespace DynoGraph;
+
+int main(int argc, char *argv[])
+{
+    // Using C-style IO, so no need to keep cout/cerr in sync
+    std::ios_base::sync_with_stdio(false);
+
+    // Reserve space for the edge list
+    // We don't know how much space we'll need, so just guess and resize as necessary
+    vector<Edge> edges;
+    edges.reserve(1024 * 1024);
+    Edge e;
+
+    // Keep reading in edges until we run out
+    while (!feof(stdin))
+    {
+        size_t rc = fread(&e, sizeof(Edge), 1, stdin);
+        if (rc) { edges.push_back(e); }
+    }
+
+    // Sort the edge list
+    std::sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) { return a.src < b.src; });
+
+    // Write to stdout
+    fwrite(&edges[0], sizeof(Edge), edges.size(), stdout);
+
+    return 0;
+}
