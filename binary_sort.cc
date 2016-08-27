d958c68e8bcbae8e15b09cef8a3a604ce675f8a6
Eric Hein <ehein6@gatech.edu>
2016-08-27 17:25:48 -0400

    Use reversed sort order for timestamps.

--- binary_sort.cc
+++ binary_sort.cc
@@ -5,6 +5,17 @@
 
 using namespace DynoGraph;
 
+bool compare(const Edge& a, const Edge& b)
+{
+    // Custom sorting order to prepare for deduplication
+    // Order by src ascending, then dest ascending, then timestamp descending
+    // This way the edge with the most recent timestamp will be picked when deduplicating
+    return (a.src != b.src) ? a.src < b.src
+         : (a.dst != b.dst) ? a.dst < b.dst
+         : (a.timestamp != b.timestamp) ? a.timestamp > b.timestamp
+         : false;
+}
+
 int main(int argc, char *argv[])
 {
     // Using C-style IO, so no need to keep cout/cerr in sync
@@ -24,7 +35,7 @@ int main(int argc, char *argv[])
     }
 
     // Sort the edge list
-    std::sort(edges.begin(), edges.end());
+    std::sort(edges.begin(), edges.end(), compare);
 
     // Write to stdout
     fwrite(&edges[0], sizeof(Edge), edges.size(), stdout);
