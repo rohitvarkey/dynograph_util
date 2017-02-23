f3d51e80640b821e668a8fd3c16954b0b1510cbf
Eric Hein <ehein6@gatech.edu>
2017-02-23 16:06:52 -0500

    Add missing include.

--- hooks/dynograph_edge_count.h
+++ hooks/dynograph_edge_count.h
@@ -17,6 +17,7 @@ extern "C" {
 // Stores edge counts for each thread
 // Do not access directly, instead use macros below
 #include <inttypes.h>
+#include <stddef.h>
 extern uint64_t* dynograph_edge_count_num_traversed_edges;
 
 #if defined(_OPENMP)
